58e14305c435352b18d7b544776917171b94d4b8
Cristian Godoy <criswxart@gmail.com>
2020-10-18 16:34:12 -0300

    solución del problema con if

--- Actividad 10 Ejercicios/Ejercicio_03/src/actividad_10/Ejercicio_03.java
+++ Actividad 10 Ejercicios/Ejercicio_03/src/actividad_10/Ejercicio_03.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package actividad_10;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +8,21 @@ package actividad_10;
  */
 public class Ejercicio_03 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        double numero;
+        Scanner t = new Scanner(System.in);
+        
+        System.out.println("Ingrese un número");
+        numero = t.nextDouble();
+        
+        if (numero > 0) {
+            System.out.println("El número [" + numero + "] es positivo");
+        }else if(numero == 0){
+            System.out.println("El número [" + (int) numero + "] es neutro");
+        }else{
+            System.out.println("El número [" + numero + "] es negativo");
+        }
     }
     
 }
