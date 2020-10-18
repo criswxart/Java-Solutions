1d4c569d73168046828c7a73c3de8e29286048fc
Cristian Godoy <criswxart@gmail.com>
2020-10-18 15:37:17 -0300

    Se creó un método para resolver el problema

--- Actividad 10 Ejercicios/Ejercicio_01/src/actidad_10/Ejercicio_01.java
+++ Actividad 10 Ejercicios/Ejercicio_01/src/actidad_10/Ejercicio_01.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package actidad_10;
-
 /**
  *
  * @author Criswxart
  */
 public class Ejercicio_01 {
 
-    /**
-     * @param args the command line arguments
-     */
+    //Método para sumar pares
+    public static void suma_pares(){
+        //Declaración de varibles principales
+        int num_par, suma, i;
+        
+        //Inicialización de varibles
+        i = 0;
+        suma = 0;
+        num_par = 2;
+        
+        //Solución
+        System.out.println("Los primeros 10 números pares son: ");
+        
+        while (i < 10) {
+            if (num_par % 2 == 0) {
+                System.out.print(num_par + "|");
+                suma = suma + num_par;
+                i++;
+            }
+            num_par++;
+        }
+        System.out.println("");
+        System.out.print("La suma de los 10 primeros números pares es: " + suma);
+        System.out.println("");
+        
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        //Llamar a la función
+        suma_pares();
+        
     }
     
 }
