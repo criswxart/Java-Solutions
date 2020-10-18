9a0f69f48050a0d1884bdd298f4e2586e39f657e
Cristian Godoy <criswxart@gmail.com>
2020-10-18 15:32:22 -0300

    primer comentario

---
+++ Actividad 10 Ejercicios/Ejercicio_01/src/actidad_10/Ejercicio_01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actidad_10;
+
+/**
+ *
+ * @author Criswxart
+ */
+public class Ejercicio_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
